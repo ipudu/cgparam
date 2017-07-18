27b0eb7180c474f4aaf78a461403f768aa15612f
Pu Du <rocketsboy@gmail.com>
2017-07-17 22:04:05 -0500

    add vmd.py

---
+++ cgparam/vmd.py
@@ -0,0 +1,9 @@
+###############################################################################
+# -*- coding: utf-8 -*-
+# cgparam: Parameterization of a coarse-grained model with Stillinger-Weber 
+#          potentials.
+#
+# Authors: Pu Du
+# 
+# Released under the GNU License
+###############################################################################
