79b5cb5fcebde8f023aa04d0c0fabd12bdac8d03
Pu Du <rocketsboy@gmail.com>
2017-07-18 17:40:35 -0500

    add plot

--- cgparam/cgparam.py
+++ cgparam/cgparam.py
@@ -11,7 +11,7 @@
 from __future__ import print_function
 import argparse
 
-from . import sw
+from . import sw, plot
 
 def get_parser():
     parser = argparse.ArgumentParser(description='cgparam: Parameterization of a \
@@ -22,10 +22,11 @@ def get_parser():
                         help=' type of task: sw,swm,plot (default: sw)')
     parser.add_argument('-p','--parameter', type=str,
                     help=' parameter files(CSV format): twobody, threebody')
-    parser.add_argument('-m','--modification', default=None, type=str,
+    parser.add_argument('-m','--modification', type=str,
                     help=' arguments to modify SW CSV file (order: paramname,number,i,j,k)')
     parser.add_argument('-s','--sw', default=None, type=str,
                     help=' output name of Stillinger-Web input for LAMMPS (default: None)')
+
     return parser
 
 def command_line_runner():
@@ -79,7 +80,12 @@ def command_line_runner():
 
                 #generated SW input for LAMMPS
                 tasker.lammps_input_writer(args['sw'], b2, b3)
-
+        
+        #plot
+        if args['task'] == 'plot':
+            tasker = plot.Plot(args['input'])
+            for filename in tasker.gofr_r:
+                tasker.plot_gr_r(filename)
 
 if __name__ == '__main__':
     command_line_runner()
