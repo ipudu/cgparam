395a04143d683b27556d6908a377991dc54382fc
Pu Du <rocketsboy@gmail.com>
2017-07-17 21:39:58 -0500

    add ssh.py

---
+++ cgparam/ssh.py
@@ -0,0 +1,28 @@
+###############################################################################
+# -*- coding: utf-8 -*-
+# cgparam: Parameterization of a coarse-grained model with Stillinger-Weber 
+#          potentials.
+#
+# Authors: Pu Du
+# 
+# Released under the GNU License
+###############################################################################
+
+class SSH(object):
+    """SSH"""
+    def __init__(self, username, hostname):
+        self.username = username
+        self.hostname = hostname
+
+    def make_bash(self, filename):
+        """make a bash file"""
+        with open(filename, 'w') as f:
+            f.write('#!/bin/bash\n\n')
+            f.write("ssh -t -t {}@{}<<'ENDSSH'\n".format(self.username, 
+                                                         self.hostname))
+            f.write('exit\n')
+            f.write('ENDSSH\n')
+
+if __name__ == '__main__':
+    connector = SSH('pdu','qb.loni.org')
+    connector.make_bash('test.sh')
