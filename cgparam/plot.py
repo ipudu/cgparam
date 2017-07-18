b0edb2fd724d0c68cb2ef1eefa1682d1c55cbb7e
Pu Du <rocketsboy@gmail.com>
2017-07-18 17:40:57 -0500

    delete test section

--- cgparam/plot.py
+++ cgparam/plot.py
@@ -8,21 +8,24 @@
 # Released under the GNU License
 ###############################################################################
 
+import os
 import numpy as np
 
 from bokeh.layouts import gridplot
 from bokeh.plotting import figure, save, output_file
 from bokeh.models import HoverTool
 
-class plot(object):
-    """plotting class"""
+from .base import Loader
 
-    def __init__(self):
-        pass
-    
+class Plot(Loader):
+    """plotting class"""
+    def __init__(self, filename):
+        super(Plot, self).__init__(filename)
+        self.gofr_r = self.constants['gofr_r']
+
     def plot_gr_r(self, filename):
         """plot radial distribution function and coordination number"""
-        data = np.loadtxt(filename+'.dat')
+        data = np.loadtxt(filename)
 
         #r, gr, c
         r = data[:,0]
@@ -48,9 +51,8 @@ class plot(object):
         p2.circle(r, c, legend="Int(r)")
         p2.line(r, c, legend="Int(r)")
         
+        #get the prefix of filename
+        base = os.path.basename(filename)
+        filename = os.path.splitext(base)[0]
         output_file(filename + ".html")
         save(gridplot(p1, p2, ncols=2, plot_width=400, plot_height=400))
-
-if __name__ == '__main__':
-    a = plot()
-    a.plot_gr_r('gofr_AA_O-O')
