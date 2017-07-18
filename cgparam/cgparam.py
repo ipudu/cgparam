bff622bfee53e58426c222f313aa75892c5c881e
Pu Du <rocketsboy@gmail.com>
2017-07-18 16:48:28 -0500

    add more arguments

--- cgparam/cgparam.py
+++ cgparam/cgparam.py
@@ -19,9 +19,11 @@ def get_parser():
                                                   Stillinger-Weber potentials')
     parser.add_argument('input', type=str, nargs='?',help='input file for cgparam')
     parser.add_argument('-t','--task', default='sw', type=str,
-                        help=' type of task: sw,plot (default: sw)')
-    parser.add_argument('-p','--parameter', default='2b.csv,3b.csv', type=str,
-                    help=' parameter files(CSV format): twobody, threebody (default: 2b.csv,3b.csv)')
+                        help=' type of task: sw,swm,plot (default: sw)')
+    parser.add_argument('-p','--parameter', type=str,
+                    help=' parameter files(CSV format): twobody, threebody')
+    parser.add_argument('-m','--modification', default=None, type=str,
+                    help=' arguments to modify SW CSV file (order: paramname,number,i,j,k)')
     parser.add_argument('-s','--sw', default=None, type=str,
                     help=' output name of Stillinger-Web input for LAMMPS (default: None)')
     return parser
@@ -34,13 +36,50 @@ def command_line_runner():
         return
 
     if args['task']:
+
+        #SW
         if args['task'] == 'sw':
             tasker = sw.SW(args['input'])
-            if args['parameter']:
+            if not args['parameter']:
+                raise StandardError("Twobody and threebody SW parammeters are not provided!")
+            else:
                 two, three = args['parameter'].split(',')
                 header_b2, b2 = tasker.csv_reader(two)
                 header_b3, b3 = tasker.csv_reader(three)
                 tasker.lammps_input_writer(args['sw'], b2, b3)
+
+        #SWM
+        if args['task'] == 'swm':
+            tasker = sw.SW(args['input'])
+            if not args['parameter']:
+                raise StandardError("Twobody and threebody SW parammeters are not provided!")
+            else:
+                two, three = args['parameter'].split(',')
+                header_b2, b2 = tasker.csv_reader(two)
+                header_b3, b3 = tasker.csv_reader(three)
+                if not args['modification']:
+                    raise StandardError("paramname, number, i, j, k \
+                                         are not provided!\n")
+                else:
+                    m = args['modification'].replace(' ', '').split(',')
+                    if len(m) == 4:
+                        paramname, number, i, j = m
+                        b2, b3 = tasker.modify_data(b2, b3, 
+                                                    paramname, number, 
+                                                    i, j)
+                    else:
+                        paramname, number, i, j, k = m
+                        b2, b3 = tasker.modify_data(b2, b3, 
+                                                    paramname, number, 
+                                                    i, j, k)
+
+                #generated new twobody and threebody parameters
+                tasker.csv_writer(b2, two)
+                tasker.csv_writer(b3, three)
+
+                #generated SW input for LAMMPS
+                tasker.lammps_input_writer(args['sw'], b2, b3)
+
 
 if __name__ == '__main__':
     command_line_runner()
