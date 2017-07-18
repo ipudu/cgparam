56b10a2d70e4fac7bd9ea0551f2aa965d47454e9
Pu Du <rocketsboy@gmail.com>
2017-07-18 15:01:52 -0500

    add util

--- cgparam/util.py
+++ cgparam/util.py
@@ -7,3 +7,14 @@
 # 
 # Released under the GNU License
 ###############################################################################
+
+import shutil
+import os
+import datetime
+
+def cp_dir(o_dir, t_dir):
+    t_dir =  t_dir + '_' + datetime.datetime.now().strftime("%y-%m-%d-%H-%M")
+    shutil.copytree(o_dir, t_dir)
+
+if __name__ == '__main__':
+    cp_dir('foo', 'bar')
