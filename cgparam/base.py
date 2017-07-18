5216b7eeb651024af60ba684cd57ce2c5cb880a5
Pu Du <rocketsboy@gmail.com>
2017-07-18 11:58:20 -0500

    add base class for loading yaml input

---
+++ cgparam/base.py
@@ -0,0 +1,18 @@
+###############################################################################
+# -*- coding: utf-8 -*-
+# cgparam: Parameterization of a coarse-grained model with Stillinger-Weber 
+#          potentials.
+#
+# Authors: Pu Du
+# 
+# Released under the GNU License
+###############################################################################
+
+from yaml import load, dump
+
+class loader(object):
+    """base class for loading input file"""
+
+    def __init__(self, filename):
+        with open(filename, 'r') as f:
+            self.constants = load(f)
